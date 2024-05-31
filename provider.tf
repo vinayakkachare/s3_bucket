6522e6c9aabd5331fc89b8312f522e088b5c6f01
vinayakkachare <161420249+vinayakkachare@users.noreply.github.com>
2024-05-31 10:01:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,13 @@
-hi hello , swpanil
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.52.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+ region = "ap-south-1"
+}
