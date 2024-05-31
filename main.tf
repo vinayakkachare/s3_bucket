00b12ae213c7d7810b32fed8921cfcc349b81d09
vinayakkachare <161420249+vinayakkachare@users.noreply.github.com>
2024-05-31 11:03:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,8 +20,11 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
 
   bucket = aws_s3_bucket.mubucket.id
-  acl    = "private"
+  acl    = "public-read"
 }
