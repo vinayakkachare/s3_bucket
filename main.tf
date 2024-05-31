a2d4ca664c3d7d9fea0ad9c2d4604ea5455f8706
vinayakkachare <161420249+vinayakkachare@users.noreply.github.com>
2024-05-31 10:39:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,5 +17,5 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
-  restrict_public_buckets = fasle
+  restrict_public_buckets = false
 }
